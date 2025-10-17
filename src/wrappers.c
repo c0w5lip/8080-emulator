cb2459c5153a87d8a1c2b2b8d9c088042fec2971
c0w5lip <152532580+c0w5lip@users.noreply.github.com>
2025-10-17 15:10:12 +0200

    refactoring

--- src/wrappers.c
+++ src/wrappers.c
@@ -98,21 +98,10 @@ void dcr(Processor *p, uint8_t *r) {
 
 
 
-void set_flag_zsp(Processor *p, uint8_t value) {
-    p->F.Z = (value == 0);
-    p->F.S = ((value & 0x80) == 0x80);
-    p->F.P = is_even(value);
-}
 
 
-void update_flags_l(Processor *p) { // logical operations
-    p->F.C = p->F.A = 0;
-    set_flag_zsp(p, p->A);
-}
 
 
-void update_flags_a(Processor *p, uint16_t r) { // arithmetical operations
-    p->F.C = (r > 0xFF);
-    set_flag_zsp(p, r & 0xFF);
-}
+
+
 
